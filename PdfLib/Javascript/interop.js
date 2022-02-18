2f14014f205e3eac46a6f623eee8e1f5ae41e8f3
Fábio Augusto Pandolfo <fabio@digitaldoc.com.br>
2022-02-18 10:49:09 -0300

    add interop and update node-signpdf reference

---
+++ PdfLib/Javascript/interop.js
@@ -0,0 +1,28 @@
+﻿const fs = require('fs');
+const plainAddPlaceholder = require('./node-signpdf/dist/helpers/index').plainAddPlaceholder;
+const signer = require('./node-signpdf/dist/signpdf').default;
+
+module.exports = (callback, arg) => {
+
+    const obj = JSON.parse(arg);
+
+    const p12Buffer = fs.readFileSync(obj.P12Path);
+
+    let pdfBuffer= fs.readFileSync(obj.PdfPath);
+
+    pdfBuffer = plainAddPlaceholder({
+        pdfBuffer: pdfBuffer,
+        reason: obj.Reason,
+        location: obj.Location,
+        name: obj.Name,
+        contactInfo: obj.ContactInfo,
+        signatureLength: obj.SignatureLength,
+        rect: obj.Rect
+    });
+
+    pdfBuffer = signer.sign(pdfBuffer, p12Buffer, { passphrase: obj.Passphrase });
+
+    fs.writeFileSync(obj.OutputPath, pdfBuffer);
+
+    callback(null, `New Signed PDF created: ${obj.OutputPath}`);
+}
