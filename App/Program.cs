ac526dc702a8c21793cba3d7234aea9b7bf28498
Fábio Augusto Pandolfo <fabio@digitaldoc.com.br>
2022-02-18 10:48:42 -0300

    update app with cert and dummy pdf

--- App/Program.cs
+++ App/Program.cs
@@ -1,5 +1,6 @@
-﻿using PdfLib;
+﻿using PdfLib.Signatures;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace App
@@ -8,12 +9,38 @@ namespace App
     {
         static void Main()
         {
-            MainAsync().GetAwaiter().GetResult();
+            try
+            {
+                MainAsync().GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
 
         static async Task MainAsync()
         {
-            var result = await PdfSigner.Sign(null, null);
+            var projectPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Javascript");
+
+            var args = new PdfSignerArgs()
+            {
+                Reason = "reason",
+                Location = "location",
+                Name = "name",
+                ContactInfo = "contact@contact.com",
+                PdfBuffer = Properties.Resources.w3dummy,
+                PdfPath = $"{Path.Combine(projectPath, "tempPdfFile.pdf")}",
+                P12Buffer = Properties.Resources.cert,
+                P12Path = $"{Path.Combine(projectPath, "tempP12File.p12")}",
+                Passphrase = "12345678",
+                OutputPath = $"{Path.Combine(projectPath, $"exported_file_{new Random().Next(5000)}.pdf")}",
+                SignatureLength = 1612,
+                Rect = new[] { 50, 50, 100, 100 },
+            };
+
+            var result = await PdfSigner.Sign(args);
+
             Console.WriteLine(result);
         }
     }
